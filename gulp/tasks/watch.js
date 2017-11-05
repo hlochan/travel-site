34824b80cb718958f032194b50203e74fb8b440c
Harsh <hlochan@gmail.com>
2017-11-05 00:41:37 -0400

    Completed Testimonials section

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 var gulp    = require('gulp'),
     watch   = require('gulp-watch'),
     browserSync = require('browser-sync').create();
-    
+
 gulp.task('watch', function(){
 
   browserSync.init({
@@ -19,9 +19,19 @@ gulp.task('watch', function(){
     gulp.start('cssInject');
   });
 
+  watch('./app/assets/scripts/**/**.js', function(){
+      // gulp.start('scripts');
+      gulp.start('scriptsRefresh');
+  });
+
+
 });
 
 gulp.task('cssInject', ['styles'] , function(){
   return gulp.src('./app/temp/styles/styles.css')
       .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+  browserSync.reload();
 });
